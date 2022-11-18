b57fe46c47770818054586cd78ca7ed4298993f2
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2022-11-17 20:09:26 -0500

    added express.json() middleware to handle req.body data

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const topicRoutes = require("./routes/topics");
 const app = express();
 
 // middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
