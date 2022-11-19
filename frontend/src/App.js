2ef91aa3bd3429131ceb4014c9165d4981db87fa
Andrew Koo <andrewkoo@Andrews-MBP-16.lan>
2022-11-19 16:45:28 -0500

    initial react set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">home</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
